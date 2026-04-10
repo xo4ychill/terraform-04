b25f9556e85ea5af8f1d962c4b7d6198fb817ee4
LebedevRP <xo4y.chill@yandex.ru>
2026-04-10 11:29:26 +0300

    main

--- src/providers.tf
+++ src/providers.tf
@@ -13,7 +13,7 @@ terraform {
 
 provider "yandex" {
 
-  service_account_key_file = pathexpand(var.service_account_key_file)
+  service_account_key_file = file(var.service_account_key_file)
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
