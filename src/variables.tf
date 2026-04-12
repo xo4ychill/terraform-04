e2071bfc7d31cda372d37760da74dad059ba2703
LebedevRP <xo4y.chill@yandex.ru>
2026-04-12 23:54:16 +0300

    tr-fix

--- src/variables.tf
+++ src/variables.tf
@@ -44,3 +44,27 @@ variable "allowed_ssh_cidr" {
 variable "environment" {
   type = string
 }
+
+# Переменная 1: одиночный IP-адрес
+variable "single_ip" {
+  description = "IP-адрес"
+  type        = string
+
+  validation {
+    condition     = can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.single_ip))
+    error_message = "Invalid IP address format. Must be a valid IPv4 address (e.g., 192.168.0.1)."
+  }
+}
+
+# Переменная 2: список IP-адресов
+variable "ip_list" {
+  description = "Список IP-адресов"
+  type        = list(string)
+
+  validation {
+    condition = alltrue([
+      for ip in var.ip_list : can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip))
+    ])
+    error_message = "All items in the list must be valid IPv4 addresses."
+  }
+}
